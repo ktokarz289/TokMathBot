60663e946995edc4a6fe1e75dd1cd8d6b9ed5e2c
ktokarz289 <kj.tokarz@gmail.com>
2016-04-26 21:12:38 -0400

    Finishing the Postfix converter

--- MathBot/PostFix/PostFixWorker.cs
+++ MathBot/PostFix/PostFixWorker.cs
@@ -8,127 +8,180 @@ using System.Threading.Tasks;
 
 namespace PostFix
 {
-    public class PostFixWorker
-    {
-		public class OperatorPriority
+	public class OperatorPriority
+	{
+		private static Dictionary<char, int> operatorPriority;
+		private static Dictionary<char, int> operatorPriorityInStack;
+
+		public OperatorPriority()
 		{
-			private static Dictionary<char, int> operatorPriority;
-
-			public OperatorPriority()
+			if (operatorPriority == null)
 			{
-				if (operatorPriority.Equals(null))
-				{
-					operatorPriority = new Dictionary<char, int>();
-					operatorPriority.Add('(', 4);
-					operatorPriority.Add(')', 0);
-					operatorPriority.Add('^', 1);
-					operatorPriority.Add('*', 2);
-					operatorPriority.Add('/', 2);
-					operatorPriority.Add('+', 3);
-					operatorPriority.Add('-', 3);
-				}
+				operatorPriority = new Dictionary<char, int>();
+				operatorPriority.Add('(', 4);
+				operatorPriority.Add(')', 5);
+				operatorPriority.Add('^', 3);
+				operatorPriority.Add('*', 2);
+				operatorPriority.Add('/', 2);
+				operatorPriority.Add('+', 1);
+				operatorPriority.Add('-', 1);
 			}
-			public int ReturnOperatorsPriority(char operatorKey)
+
+			if (operatorPriorityInStack == null)
 			{
-				int priority = -1;
-
-				try
-				{
-					operatorPriority.TryGetValue(operatorKey, out priority);
-				}
-				catch (Exception e)
-				{
-					Console.WriteLine(e.Message);
-				}
-
-				return priority;
+				operatorPriorityInStack = new Dictionary<char, int>();
+				operatorPriorityInStack.Add('(', 0);
+				operatorPriorityInStack.Add(')', 5);
+				operatorPriorityInStack.Add('^', 3);
+				operatorPriorityInStack.Add('*', 2);
+				operatorPriorityInStack.Add('/', 2);
+				operatorPriorityInStack.Add('+', 1);
+				operatorPriorityInStack.Add('-', 1);
 			}
 		}
-
-		public class PostfixConverter
+		public int ReturnOperatorsPriority(char operatorKey)
 		{
-			private string infixNotation;
-			private string postfixNotation;
-			public OperatorPriority op;
+			int priority = -1;
 
-			public PostfixConverter(string infix)
+			try
 			{
-				infixNotation = infix;
-				op = new OperatorPriority();
+				operatorPriority.TryGetValue(operatorKey, out priority);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
 			}
 
-			public void Converter()
+			return priority;
+		}
+
+		public int ReturnOperatorsPriorityInStack(char operatorKey)
+		{
+			int priority = -1;
+
+			try
 			{
-				Stack operators = new Stack();
-				foreach (char c in infixNotation)
+				operatorPriorityInStack.TryGetValue(operatorKey, out priority);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+
+			return priority;
+		}
+	}
+
+	public class PostfixConverter
+	{
+		private string infixNotation;
+		private string postfixNotation;
+		public OperatorPriority op;
+
+		public PostfixConverter(string infix)
+		{
+			infixNotation = infix;
+			op = new OperatorPriority();
+		}
+
+		public void Converter()
+		{
+			Stack operators = new Stack();
+			foreach (char c in infixNotation)
+			{
+				if (CheckIfOperand(c.ToString()))
 				{
-					if (CheckIfOperand(c.ToString()))
-					{
-						postfixNotation += c;
-					}
-					else if (operators.Count == 0)
-					{
-						operators.Push(c);
-					}
-					else
-					{
-						HandleStackOperators(c, ref operators);
-					}
+					postfixNotation += c;
 				}
-			}
-
-			public bool CheckIfOperand(string potentialOperand)
-			{
-				bool success = false;
-				string pattern = @"^[a-zA-Z0-9]";
-
-				if (Regex.IsMatch(potentialOperand, pattern))
+				else if (operators.Count == 0)
 				{
-					success = true;
-				}
-
-				return success;
-			}
-
-			public void HandleStackOperators(char currOperator, ref Stack operators)
-			{
-				if (OperatorGreaterThanTopOfStack(currOperator, (char)operators.Peek()))
-				{
-					operators.Push(currOperator);
+					operators.Push(c);
 				}
 				else
 				{
-					postfixNotation += operators.Pop();
-					while (!OperatorGreaterThanTopOfStack(currOperator, (char)operators.Peek()))
-						postfixNotation += operators.Pop();
-
-					operators.Push(currOperator);
+					HandleStackOperators(c, ref operators);
 				}
 			}
 
-			public bool OperatorGreaterThanTopOfStack(char currOperator, char topStackOperator)
-			{
-				bool success = true;
-				int currOperatorPriority = op.ReturnOperatorsPriority(currOperator);
-				int topOperatorPriority = op.ReturnOperatorsPriority(topStackOperator);
-				if (currOperator < topOperatorPriority)
-				{
-					success = false;
-				}
-				return success;
-			}
-
-			public string ConvertAndReturn()
-			{
-				Converter();
-				return postfixNotation;
-			}
-
-			public string ReturnPostfix()
-			{
-				return postfixNotation;
-			}
-
+			while (operators.Count > 0)
+				PopThroughStack(ref operators);
 		}
+
+		public bool CheckIfOperand(string potentialOperand)
+		{
+			bool success = false;
+			string pattern = @"^[a-zA-Z0-9]";
+
+			if (Regex.IsMatch(potentialOperand, pattern))
+			{
+				success = true;
+			}
+
+			return success;
+		}
+
+		public void HandleStackOperators(char currOperator, ref Stack operators)
+		{
+			if (OperatorGreaterThanTopOfStack(currOperator, (char)operators.Peek()))
+			{
+				operators.Push(currOperator);
+			}
+			else
+			{
+				string character = operators.Pop().ToString();
+				if (character != "(" && character != ")")
+				{
+					postfixNotation += character;
+					while (operators.Count > 0 && !OperatorGreaterThanTopOfStack(currOperator, (char)operators.Peek()))
+					{
+						PopThroughStack(ref operators);
+					}
+				}
+				else
+				{
+					do
+					{
+						PopThroughStack(ref operators);
+					}
+					while (operators.Peek().ToString() != "(");
+					operators.Pop();
+				}
+
+				operators.Push(currOperator);
+			}
+		}
+
+		public void PopThroughStack(ref Stack operators)
+		{
+			string character = operators.Pop().ToString();
+			if (character != "(" && character != ")")
+			{
+				postfixNotation += character;
+			}
+		}
+
+		public bool OperatorGreaterThanTopOfStack(char currOperator, char topStackOperator)
+		{
+			bool success = false;
+			int currOperatorPriority = op.ReturnOperatorsPriority(currOperator);
+			int topOperatorPriority = op.ReturnOperatorsPriorityInStack(topStackOperator);
+			if (currOperatorPriority > topOperatorPriority)
+			{
+				success = true;
+			}
+			return success;
+		}
+
+		public string ConvertAndReturn()
+		{
+			Converter();
+			return postfixNotation;
+		}
+		 
+		public string ReturnPostfix()
+		{
+			return postfixNotation;
+		}
+
 	}
 }
--- MathBot/PostFixUnitTests/PostfixTests.cs
+++ MathBot/PostFixUnitTests/PostfixTests.cs
@@ -1,14 +1,55 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-	 
+using PostFix;
+
 namespace PostFixUnitTests
 {
 	[TestClass]
 	public class PostfixTests
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void SimpleEquation()
 		{
+			PostfixConverter converter = new PostfixConverter("1+2");
+			string postfix = converter.ConvertAndReturn();
+
+			Assert.AreEqual(postfix, "12+");
+		}
+
+		[TestMethod]
+		public void ComplexEqn1()
+		{
+			PostfixConverter converter = new PostfixConverter("1+2*3");
+			string postfix = converter.ConvertAndReturn();
+
+			Assert.AreEqual(postfix, "123*+");
+		}
+
+		[TestMethod]
+		public void ComplexEqn1Inverse()
+		{
+			PostfixConverter converter = new PostfixConverter("2*3+1");
+			string postfix = converter.ConvertAndReturn();
+
+			Assert.AreEqual(postfix, "23*1+");
+		}
+
+		[TestMethod]
+		public void ComplexEqn2()
+		{
+			PostfixConverter converter = new PostfixConverter("2*3+6*(3+4)");
+			string postfix = converter.ConvertAndReturn();
+
+			Assert.AreEqual(postfix, "23*634+*+");
+		}
+
+		[TestMethod]
+		public void ComplexEqn3()
+		{
+			PostfixConverter converter = new PostfixConverter("(3+4)*2*3+6");
+			string postfix = converter.ConvertAndReturn();
+
+			Assert.AreEqual(postfix, "34+2*3*6+");
 		}
 	}
 }
